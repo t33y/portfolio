309f92be95f1fcce7a826ef31e9d77b182612e0c
t33y <omotayoolarewaju@gmail.com>
2024-06-24 13:00:43 +0100

    updates readme

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import shoutoutImg from "../images/shoutout.png";
-import shoutoutImgl from "../images/shoutoutlight.png";
+import lykdatImg from "../images/lykdat.png";
+import lykdatImgl from "../images/lykdatlight.png";
 import wordgameImg from "../images/wordgame.png";
 import wordgameImgl from "../images/wordgamelight.png";
 import noteappImg from "../images/noteapp.png";
@@ -51,13 +51,38 @@ export const experiencesData = [
     title: "Solutions Engineer",
     company: "Halliburton",
     description:
-      "After graduation (Mechanical Engineering 2.1 hons). I worked as a solutions engineer.I was responsible for planning, testing, designing and delivering software application solutions",
+      "After graduation (Mechanical Engineering 2.1 hons). I worked as a solutions engineer.I was responsible for testing, designing and delivering software and web application solutions",
     icon: React.createElement(CgWorkAlt),
     date: "2014 - 2021",
   },
 ] as const;
 
 export const projectsData = [
+  {
+    title: "Lykdat",
+    description:
+      "Lykdat makes it easy to find and purchase similar styles of an outfit with just an image. With powerful search features that helps you find what you want even when they don't have the right words.",
+    tags: ["React", "Next.js", "Ant Design", "TypeScript"],
+    imageUrl: { dark: lykdatImg, light: lykdatImgl },
+    link: "https://lykdat.com/",
+  },
+
+  {
+    title: "PriceWhistle",
+    description:
+      "Ever wished you could get that trendy outfit at a lower price? PriceWhistle is your personal fashion companion that keeps you in the loop on the latest deals on your favourite outfit",
+    tags: ["React", "Next.js", "TypeScript"],
+    imageUrl: { dark: pricewhistleImg, light: pricewhistleImgl },
+    link: "https://pricewhistle.com/",
+  },
+  {
+    title: "SimpleVideoPlayer",
+    description:
+      "SimpleVideoPlayer.com is a Video player that runs on the Browser. It strictly plays video files that exist on your local computer. It offers features similar to VLC such as amplified volume (you can increase volume to 400%), subtitle files (you can enable subtitles as you watch a video), keyboard controls (up and down for volume, etc.).Sometimes you have videos from untrusted sources and you are worried about the safety of playing them with any app on your machine.Because browsers like Chrome, and Brave have sandboxing. Even though they are also not 100% secure, you are more assured when using them. Also browsers are updated more often than a lot of video players.",
+    tags: ["React", "Next.js", "TypeScript"],
+    imageUrl: { dark: simplevideoplayerImg, light: simplevideoplayerImgl },
+    link: "https://simplevideoplayer.com/",
+  },
   {
     title: "NoteApp",
     description:
@@ -80,30 +105,6 @@ export const projectsData = [
     tags: ["React", "Css", "Tailwind", "MaterialUi"],
     imageUrl: { dark: wordgameImg, light: wordgameImgl },
     link: "https://t33y.github.io/hangman/",
-  },
-  {
-    title: "PriceWhistle",
-    description:
-      "Ever wished you could get that trendy outfit at a lower price? PriceWhistle is your personal fashion companion that keeps you in the loop on the latest deals on your favourite outfit",
-    tags: ["React", "Next.js", "TypeScript"],
-    imageUrl: { dark: pricewhistleImg, light: pricewhistleImgl },
-    link: "https://pricewhistle.com/",
-  },
-  {
-    title: "SimpleVideoPlayer",
-    description:
-      "SimpleVideoPlayer.com is a Video player that runs on the Browser. It strictly plays video files that exist on your local computer. It offers features similar to VLC such as amplified volume (you can increase volume to 400%), subtitle files (you can enable subtitles as you watch a video), keyboard controls (up and down for volume, etc.).Sometimes you have videos from untrusted sources and you are worried about the safety of playing them with any app on your machine.Because browsers like Chrome, and Brave have sandboxing. Even though they are also not 100% secure, you are more assured when using them. Also browsers are updated more often than a lot of video players.",
-    tags: ["React", "Next.js", "TypeScript"],
-    imageUrl: { dark: simplevideoplayerImg, light: simplevideoplayerImgl },
-    link: "https://simplevideoplayer.com/",
-  },
-  {
-    title: "ShoutOut",
-    description:
-      "A social web app where users can share uplifting shout outs,  like and unlike posts and have a following. Sign in and make a shout out",
-    tags: ["React", "Next.js", "NextAuth", "Tailwind", "TypeScript", "TRPC"],
-    imageUrl: { dark: shoutoutImg, light: shoutoutImgl },
-    link: "https://shout-out-one-lac.vercel.app/",
   },
 ] as const;
 
